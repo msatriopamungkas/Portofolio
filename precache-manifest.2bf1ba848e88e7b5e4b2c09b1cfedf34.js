bc8f4fbcd6049c7db09943ad8f94f536c2bd29e9
Satrio Pamungkas <satriopmgks007@gmail.com>
2019-12-15 02:15:48 +0700

    Updates

---
+++ precache-manifest.2bf1ba848e88e7b5e4b2c09b1cfedf34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c4d4f065b8eb7f934cdc6590074de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23af97d0fbb6be283045",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "db93dd7f64ba99f5a214",
+    "url": "/static/js/2.4db7a91d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4db7a91d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23af97d0fbb6be283045",
+    "url": "/static/js/main.db2a4efe.chunk.js"
+  },
+  {
+    "revision": "633ad3278563d8138e7d",
+    "url": "/static/js/runtime-main.d725bfa7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
